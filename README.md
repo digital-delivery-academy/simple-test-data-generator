abbcc51ed5f381b6084ba25a4a1c973607bb00e1
Kurt Plaschke <k.plaschke@hull.ac.uk>
2021-01-12 15:46:15 +0000

    added tests to increase cov, added LICENSE, contrib. with me included, and readme shell

---
+++ README.md
@@ -0,0 +1,14 @@
+# Test Data Generator
+![TestData CI](https://github.com/digital-delivery-academy/simple-test-data-generator/workflows/TestData%20CI/badge.svg)
+
+[![Coverage](https://sonarcloud.io/api/project_badges/measure?project=digital-delivery-academy_simple-test-data-generator&metric=coverage)](https://sonarcloud.io/dashboard?id=digital-delivery-academy_simple-test-data-generator)
+[![Quality Gate Status](https://sonarcloud.io/api/project_badges/measure?project=digital-delivery-academy_simple-test-data-generator&metric=alert_status)](https://sonarcloud.io/dashboard?id=digital-delivery-academy_simple-test-data-generator)
+[![Security Rating](https://sonarcloud.io/api/project_badges/measure?project=digital-delivery-academy_simple-test-data-generator&metric=security_rating)](https://sonarcloud.io/dashboard?id=digital-delivery-academy_simple-test-data-generator)
+
+## Documentation
+- A full reference guide is here: https://digital-delivery-academy.github.io/selenium-pom-framework/index.html
+- Technical reference documentation (javadocs) are here: https://digital-delivery-academy.github.io/selenium-pom-framework/javadoc-1.0.14
+
+## Example checks/project
+
+See https://github.com/digital-delivery-academy/selenium-pom-example repository for an example of how to write Selenium checks using the Page Object Model.  This repository uses this framework.
---
+++ src/test/java/utils/JsonUtilsTests.java
@@ -0,0 +1,70 @@
+package utils;
+
+import org.junit.jupiter.api.Test;
+import uk.co.evoco.utils.JsonUtils;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+
+public class JsonUtilsTests {
+
+    private static final String FILE = "fixtures/test.json";
+
+    @Test
+    public void testFromFileWithTestClass() throws IOException {
+        File file = loadFile(FILE);
+        TestObject testObject = JsonUtils.fromFile(file, TestObject.class);
+        assertThat(testObject.getField1(), is("test1"));
+    }
+
+    @Test
+    public void testFromFileWithTestClassFromInputStream() throws IOException {
+        InputStream is = ClassLoader.getSystemResourceAsStream(FILE);
+        TestObject testObject = JsonUtils.fromFile(is, TestObject.class);
+        assertThat(testObject.getField1(), is("test1"));
+    }
+
+    @Test
+    public void testFromFileWithTestClassFromString() throws IOException {
+        TestObject testObject = JsonUtils.fromFile(ClassLoader.getSystemResource(FILE).getPath(),
+                TestObject.class);
+        assertThat(testObject.getField1(), is("test1"));
+    }
+
+    @Test
+    public void testToStringOnObject() throws IOException {
+        TestObject testObject = JsonUtils.fromFile(ClassLoader.getSystemResource(FILE).getPath(),
+                TestObject.class);
+        assertThat(JsonUtils.toString(testObject), is("{\"field1\":\"test1\",\"field2\":\"test2\"}"));
+    }
+
+    private File loadFile(String path) throws IOException {
+        File file = new File(path);
+        if (file.isFile()) {
+            return file;
+        } else {
+            try {
+                return new File(ClassLoader.getSystemResource(path).getPath());
+            } catch(NullPointerException e) {
+                throw new IOException("File could not be found: " + path);
+            }
+        }
+    }
+}
+
+class TestObject {
+    private String field1;
+    private String field2;
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+}
